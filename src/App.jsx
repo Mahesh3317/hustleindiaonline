ad31a27d16a5b4a6e7dfdaf20b137844799b02aa
Mahesh3317 <maheshsharma3317js@gmail.com>
2025-07-20 11:19:41 +0530

    Added google analytics-V1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Routes from "./Routes";
 
 function App() {
-  return (
-    <Routes />
-  );
+  useEffect(() => {
+    // Load Google Analytics gtag.js
+    const script1 = document.createElement("script");
+    script1.setAttribute("async", "");
+    script1.src = "https://www.googletagmanager.com/gtag/js?id=G-6XWKW8PXWG";
+    document.head.appendChild(script1);
+
+    // Google Analytics config
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-6XWKW8PXWG');
+    `;
+    document.head.appendChild(script2);
+  }, []);
+
+  return <Routes />;
 }
 
 export default App;
